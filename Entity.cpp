1a170fdca79badcf274ed644862850456724231a
aalyaqoub <aalyaqoub2001@gmail.com>
2021-11-12 00:40:16 -0500

    Completed parts of game and entity

--- Entity.cpp
+++ Entity.cpp
@@ -8,18 +8,19 @@ using namespace std;
 Player::Player(const Cell & cell, const EntityCounter & counter): // TODO
 m_cell(cell),
 m_counter(counter){
-  // TODO
+  m_counter.increment();
 }
 
 Player::Player(const Player & other): // TODO
 m_cell(other.m_cell),
 m_counter(other.m_counter)
 {
+  m_counter.increment();
 }
 
 Player::~Player()
 {
-  // TODO
+  m_counter.decrement();
 }
 
 void
@@ -27,22 +28,18 @@ Player::update(Direction input_direction, size_t worldRows, size_t worldCols)
 {
   if(input_direction == Direction::east) {
     if(m_cell.col + 1 <= worldCols) {
-      m_cell.col + 1;
+      m_cell.col = m_cell.col + 1;
     }
   }
   if(input_direction == Direction::west) {
-    if(m_cell.col - 1 >= 0) {
-      m_cell.col - 1;
-    }
+    m_cell.col = m_cell.col - 1;
   }
   if(input_direction == Direction::north) {
-    if(m_cell.row - 1 >= 0) {
-      m_cell.row - 1;
-    }
+    m_cell.row = m_cell.row - 1;
   }
   if(input_direction == Direction::south) {
     if(m_cell.row + 1 <= worldRows) {
-      m_cell.row + 1;
+      m_cell.row = m_cell.row + 1;
     }
   }
 }
@@ -57,23 +54,23 @@ Player::get_cell() const
 
 RobotOrDebris::RobotOrDebris(const Cell & cell, bool isDebris, const EntityCounter & counter):
 m_cell(cell),
-m_counter(counter),
-m_isDebris(isDebris)
+m_isDebris(isDebris),
+m_counter(counter)
 {
-
+  m_counter.increment();
 }
 
 RobotOrDebris::RobotOrDebris(const RobotOrDebris & other):
 m_cell(other.m_cell),
-m_counter(other.m_counter),
-m_isDebris(other.m_isDebris) // TODO
+m_isDebris(other.m_isDebris), // TODO
+m_counter(other.m_counter)
 {
-  // TODO
+  m_counter.increment();
 }
 
 RobotOrDebris::~RobotOrDebris()
 {
-  // TODO
+  m_counter.decrement();
 }
 
 void
@@ -83,24 +80,24 @@ RobotOrDebris::update(const Player & player)
     if(!(m_cell.row == player.get_cell().row && m_cell.col == player.get_cell().col)) {
     if(m_cell.row == player.get_cell().row) {
       if(m_cell.col < player.get_cell().col)
-      m_cell.col + 1;
-      else { m_cell.col - 1;}
+      m_cell.col = m_cell.col + 1;
+      else { m_cell.col = m_cell.col - 1;}
     }
     else if(m_cell.col == player.get_cell().col) {
       if(m_cell.row < player.get_cell().row)
-      m_cell.row + 1;
-      else { m_cell.row - 1;} 
+      m_cell.row = m_cell.row + 1;
+      else { m_cell.col = m_cell.row - 1;} 
     }
     else {
       if(m_cell.row + m_cell.col % 2 == 0) {
         if(m_cell.row < player.get_cell().row)
-      m_cell.row + 1;
-      else { m_cell.row - 1;}
+      m_cell.row = m_cell.row + 1;
+      else { m_cell.row = m_cell.row - 1;}
     }
     else {
        if(m_cell.col < player.get_cell().col)
-      m_cell.col + 1;
-      else { m_cell.col - 1;}
+      m_cell.col = m_cell.col + 1;
+      else { m_cell.col = m_cell.col - 1;}
     }
     }
       }
--- Game.cpp
+++ Game.cpp
@@ -26,6 +26,56 @@ const char * errmsg_direction = "Invalid direction input.";
 void
 Game::input_and_read_world_from_file(const EntityCounter & counter)
 {
+  cout << msg_filename << endl;
+  string fileName;
+  cin >> fileName;
+  ifstream infile(fileName + ".txt");
+
+  if (!infile) {
+    throw invalid_argument(errmsg_world_file);
+  }
+
+  string line;
+  size_t row = 0;
+  size_t col = 0;
+  size_t lineCounter = 0;
+  size_t colCounter = 0;
+  while (getline(infile, line)) {
+    ++lineCounter;
+    for (char c : line) {
+      if (c == 'x') {
+        const Cell c{row, col};
+        RobotOrDebris tempX(c, true, counter);
+        m_robotsAndDebris.push_back(tempX);
+      } else if (c == 'r') {
+        const Cell c{row, col};
+        RobotOrDebris tempR(c, false, counter);
+        m_robotsAndDebris.push_back(tempR);
+      } else if (c == 'p') {
+        const Cell c{row, col};
+        Player tempP(c, counter);
+        m_player.push_back(tempP);
+      } else if (c != '.') {
+        throw invalid_argument(errmsg_world_char);
+      }
+      ++col;
+    }
+    if (colCounter != col || col == 0) {
+      throw invalid_argument(errmsg_world_shape);
+    }
+    colCounter = col;
+    col = 0;
+    ++row;
+  }
+  m_worldRows = row - 1;
+  m_worldCols = col - 1;
+
+  if (m_player.size() == 0) {
+    throw invalid_argument(errmsg_world_player_missing);
+  }
+  if (m_player.size() > 1) {
+    throw invalid_argument(errmsg_world_player_multiple);
+  }
   // TODO: prompt user for filename
   
   // TOOD: read and validate world from file
@@ -34,12 +84,45 @@ Game::input_and_read_world_from_file(const EntityCounter & counter)
 void
 Game::display_world() const
 {
-  // TODO: display world
+  string world = "";
+  for (size_t row = 0; row < m_worldRows; ++row) {
+    for (size_t col = 0; col < m_worldCols; ++col) {
+      Cell currentC{row, col};
+      bool inRD = false;
+      bool isD = false;
+      bool inP = false;
+      for (RobotOrDebris rd : m_robotsAndDebris) {
+        inRD = inRD || rd.get_cell() == currentC;
+        if (inRD) {
+          isD = rd.is_debris();
+        }
+      }
+      for (Player p : m_player) {
+        inP = inP || p.get_cell() == currentC;
+      }
+      if (inRD && isD) {
+        world = world + "x";
+      } else if (inRD) {
+        world = world + "r";
+      } else if (inP) {
+        world = world + "p";
+      } else {
+        world = world + ".";
+      }
+    }
+    if (row + 1 < m_worldRows) {
+      world = world + "\n";
+    }
+  }
+  cout << world << endl;
 }
 
 GameStatus
 Game::input_next_move_and_update()
 {
+  string direction;
+  cout << msg_direction << endl;
+  cin >> direction;
   // TODO: prompt user and input direction
 
   // TODO: update player and robot locations
