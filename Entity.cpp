da4a06ba1dab5229828f41c723b807559acdcdc1
Ali Rana <alimasoodrana4@gmail.com>
2021-11-11 21:43:24 -0500

    finished entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -55,12 +55,18 @@ Player::get_cell() const
 
 
 
-RobotOrDebris::RobotOrDebris(const Cell & cell, bool isDebris, const EntityCounter & counter) // TODO
+RobotOrDebris::RobotOrDebris(const Cell & cell, bool isDebris, const EntityCounter & counter):
+m_cell(cell),
+m_counter(counter),
+m_isDebris(isDebris)
 {
-  // TODO
+
 }
 
-RobotOrDebris::RobotOrDebris(const RobotOrDebris & other) // TODO
+RobotOrDebris::RobotOrDebris(const RobotOrDebris & other):
+m_cell(other.m_cell),
+m_counter(other.m_counter),
+m_isDebris(other.m_isDebris) // TODO
 {
   // TODO
 }
@@ -73,8 +79,34 @@ RobotOrDebris::~RobotOrDebris()
 void
 RobotOrDebris::update(const Player & player)
 {
-  // TODO
+  if(!m_isDebris) {
+    if(!(m_cell.row == player.get_cell().row && m_cell.col == player.get_cell().col)) {
+    if(m_cell.row == player.get_cell().row) {
+      if(m_cell.col < player.get_cell().col)
+      m_cell.col + 1;
+      else { m_cell.col - 1;}
+    }
+    else if(m_cell.col == player.get_cell().col) {
+      if(m_cell.row < player.get_cell().row)
+      m_cell.row + 1;
+      else { m_cell.row - 1;} 
+    }
+    else {
+      if(m_cell.row + m_cell.col % 2 == 0) {
+        if(m_cell.row < player.get_cell().row)
+      m_cell.row + 1;
+      else { m_cell.row - 1;}
+    }
+    else {
+       if(m_cell.col < player.get_cell().col)
+      m_cell.col + 1;
+      else { m_cell.col - 1;}
+    }
+    }
+      }
+    }
 }
+
 
 const Cell &
 RobotOrDebris::get_cell() const
