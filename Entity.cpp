70843aac2d87b7083b24f65ee99304dd3d6388fe
Ali Rana <alimasoodrana4@gmail.com>
2021-11-11 21:17:12 -0500

    filled player constructors

--- Entity.cpp
+++ Entity.cpp
@@ -13,9 +13,8 @@ m_counter(counter){
 
 Player::Player(const Player & other): // TODO
 m_cell(other.m_cell),
-m_counter(//get other couter)
+m_counter(other.m_counter)
 {
-  // TODO
 }
 
 Player::~Player()
@@ -26,7 +25,26 @@ Player::~Player()
 void
 Player::update(Direction input_direction, size_t worldRows, size_t worldCols)
 {
-  // TODO
+  if(input_direction == Direction::east) {
+    if(m_cell.col + 1 <= worldCols) {
+      m_cell.col + 1;
+    }
+  }
+  if(input_direction == Direction::west) {
+    if(m_cell.col - 1 >= 0) {
+      m_cell.col - 1;
+    }
+  }
+  if(input_direction == Direction::north) {
+    if(m_cell.row - 1 >= 0) {
+      m_cell.row - 1;
+    }
+  }
+  if(input_direction == Direction::south) {
+    if(m_cell.row + 1 <= worldRows) {
+      m_cell.row + 1;
+    }
+  }
 }
 
 const Cell &
